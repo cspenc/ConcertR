f3ce2c3359d5abe92115f18aa6297dcb76f62265
cspenc <crs992@gmail.com>
2017-06-14 23:37:23 -0400

    working through users, need to fix relationships, added bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first, :last, :username, :bio, :password, :password_conf)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :reviews
-  has_many :concerts through: :reviews
+  # has_many :concerts through: :reviews
 end
