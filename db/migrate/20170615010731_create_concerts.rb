a87cfeddc683d768ec86356f91a69ba882e15f85
cspenc <crs992@gmail.com>
2017-06-14 21:10:48 -0400

    first commit

---
+++ db/migrate/20170615010731_create_concerts.rb
@@ -0,0 +1,10 @@
+class CreateConcerts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :concerts do |t|
+      t.string :review
+      t.string :user
+
+      t.timestamps
+    end
+  end
+end
