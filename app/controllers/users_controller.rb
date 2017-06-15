d40763a0cbc7083818dbdf35f3bff34332ed30e6
cspenc <crs992@gmail.com>
2017-06-15 00:28:59 -0400

    show, edit, new pages updated, can create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,42 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if user_params[:password] == user_params[:password_confirmation] && @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if user_params[:password] == user_params[:password_confirmation] && @user.update
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first, :last, :username, :bio, :password, :password_conf)
+    params.require(:user).permit(:first, :last, :username, :bio, :password, :password_confirmation)
   end
 
 end
