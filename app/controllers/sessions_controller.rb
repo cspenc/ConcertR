d38ea8abcba12fb1bf20e5fde642de775bd61ef2
cspenc <crs992@gmail.com>
2017-06-22 15:21:29 -0400

    work on sessions login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: session_params[:username])
+    if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      redirect_to welcome_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :reviews, :concerts
+  resources :users, :reviews, :concerts, :sessions
 
   root 'sessions#home'
   get '/login', to: 'sessions#new'
